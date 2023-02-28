da0aed47bcda93f4f590453f23ac570421f6f682
Mta-gh <mathias.greenhalgh@gmail.com>
2023-02-28 16:07:03 +0100

    3d second object

--- dev/js/three.js
+++ dev/js/three.js
@@ -4,7 +4,11 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 1000, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const texture = new THREE.TextureLoader().load( '../img/simone-welcome.png' );
+// flip vertical
 texture.flipY = false;
+// flip horizontally
+texture.wrapS = THREE.RepeatWrapping;
+texture.repeat.x = - 1;
 
 
 var renderer = new THREE.WebGLRenderer();
@@ -17,6 +21,11 @@ var material = new THREE.MeshBasicMaterial( { map: texture, transparent:true, si
 var cylinder = new THREE.Mesh( geometry, material );
 scene.add( cylinder );
 
+var geometry2 = new THREE.CylinderGeometry( 2, 2, 10, 15, 1, true );
+var material2 = new THREE.MeshBasicMaterial( { color: 0xfffff , wireframe: true}  );
+var cylinder2 = new THREE.Mesh( geometry2, material2 );
+scene.add( cylinder2 );
+
 camera.position.z = 20;
 
 
@@ -24,6 +33,8 @@ function render() {
 	requestAnimationFrame(render);
     /* cylinder.rotation.z += 0.01; */
   cylinder.rotation.y += 0.01;
+  cylinder2.rotation.y -= 0.01;
+
 	renderer.render(scene, camera);
 }
 render();
